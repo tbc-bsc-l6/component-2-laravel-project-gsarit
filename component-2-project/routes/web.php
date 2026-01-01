26db4de769bb43149ed21ef9d1e52c2ee6921e05
gsarit <gsarita22@tbc.edu.np>
2026-01-01 17:56:08 +0545

    Update student routes

--- component-2-project/routes/web.php
+++ component-2-project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+
+use App\Http\Controllers\StudentModuleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+// Only for authenticated students
+Route::middleware(['auth'])->group(function() {
+
+    Route::get('/student/modules', [StudentModuleController::class, 'index'])
+        ->name('student.modules.index');
+
+    Route::post('/student/modules/{module}/enrol', [StudentModuleController::class, 'enrol'])
+        ->name('student.modules.enrol');
 });
 
 require __DIR__.'/auth.php';
