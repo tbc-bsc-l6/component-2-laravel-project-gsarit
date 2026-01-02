72358488cc4b7604cf36fe4cc400102819ab9a3d
gsarit <gsarita22@tbc.edu.np>
2026-01-02 20:49:42 +0545

    Update module_user migration

--- component-2-project/database/migrations/2025_12_31_093935_create_module_user_table.php
+++ component-2-project/database/migrations/2025_12_31_093935_create_module_user_table.php
@@ -13,13 +13,28 @@ return new class extends Migration
     {
         Schema::create('module_user', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('module_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->timestamp('enrolled_at')->nullable();
+            $table->enum('pass_status', ['PASS', 'FAIL'])->nullable();
+            $table->timestamp('completed_at')->nullable();
+
             $table->timestamps();
+
+            $table->unique(['module_id', 'user_id']);
         });
     }
 
     /**
-     * Reverse the migrations.
-     */
+      * Reverse the migrations.
+      */
     public function down(): void
     {
         Schema::dropIfExists('module_user');
